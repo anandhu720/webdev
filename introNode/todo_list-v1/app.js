eaf009ff5cf32dfc3a0f2d952a05c61f44e7264f
anandhu720 <anandhu4310@gmail.com>
2021-02-21 16:47:57 +0530

    feb 21

---
+++ introNode/todo_list-v1/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req, res) {
+    var today = new Date();
+
+    if (today.getDay() === 6 || today.getDay() === 0) {
+        res.sendFile(__dirname+"/index.html");
+    } else {
+        res.sendFile(__dirname+"/index.html");
+    }
+});
+
+app.listen(3000, function() {
+    console.log("port is opened in 3000");
+});
