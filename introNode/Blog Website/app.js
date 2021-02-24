1caaf7b9e04e444d56d8a2ea283c0ba647e8f0a0
anandhu720 <anandhu4310@gmail.com>
2021-02-24 09:21:04 +0530

    feb 24

--- introNode/Blog Website/app.js
+++ introNode/Blog Website/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
     "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -18,8 +19,13 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let postArray = [];
+
 app.get("/", function(req, res) {
-    res.render("home", { homeContent1: homeStartingContent });
+    res.render("home", {
+        homeContent1: homeStartingContent,
+        postFull: postArray,
+    });
 });
 
 app.get("/contact", function(req, res) {
@@ -32,6 +38,29 @@ app.get("/about", function(req, res) {
 
 app.get("/compose", function(req, res) {
     res.render("compose");
+});
+
+app.get("/posts/:postName", function(req, res) {
+    let req_post = _.lowerCase(req.params.postName);
+    postArray.forEach(function(post) {
+        let arrayTitle = _.lowerCase(post.posttitle);
+        let arrayContent = _.lowerCase(post.postcontent);
+        if (req_post === arrayTitle) {
+            res.render("post", {
+                postHeading: arrayTitle,
+                postContent: arrayContent,
+            });
+        }
+    });
+});
+
+app.post("/compose", function(req, res) {
+    let post = {
+        posttitle: req.body.postTitle,
+        postcontent: req.body.postContent,
+    };
+    postArray.push(post);
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
