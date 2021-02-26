868b15be13b4f1efacccd7473dfe2e957775a3fb
anandhu720 <anandhu4310@gmail.com>
2021-02-27 00:28:48 +0530

    root is connected with database,other remain

--- introNode/todo_list-v1/app.js
+++ introNode/todo_list-v1/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/public/js/date.js");
 
 const app = express();
 
@@ -9,26 +8,80 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+});
 
 // let items = [];
 // let workItems = [];
 
-app.get("/", function(req, res) {
-    let day = date.findDate();
-    res.render("list", { listTitle: day, newListItem: items });
+//creating schema
+const itemsSchema = {
+    name: String,
+};
+
+const listSchema = {
+    name: String,
+    items: [itemsSchema],
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+const List = mongoose.model("List", listSchema);
+
+const item1 = new Item({
+    name: "Welcome to To-Do List",
 });
 
-app.get("/work", function(req, res) {
-    res.render("list", { listTitle: "work List", newListItem: workItems });
+const defaultItems = [item1];
+
+app.get("/", function(req, res) {
+    Item.find({}, function(err, result) {
+        if (result.length === 0) {
+            Item.insertMany(defaultItems, function(err) {
+                if (err)
+                    console.log(
+                        "error Opppss!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                    );
+                else console.log("hurahh Success!!!!!!!!!!!");
+            });
+            res.redirect("/");
+        } else {
+            res.render("list", { listTitle: "Today", newListItem: result });
+        }
+    });
+});
+
+app.get("/:coustomListName", function(req, res) {
+    const coustomlistname = req.params.coustomListName;
+
+    const list = {
+        name: coustomlistname,
+        items: defaultItems,
+    };
+
+    list.save();
+});
+
+app.post("/delete", function(req, res) {
+    const checkedItemName = req.body.checkbox;
+    Item.deleteOne({ _id: checkedItemName }, function(err) {
+        if (err) console.log("error in deleting element");
+        else console.log("deleted checkedElement");
+    });
+    res.redirect("/");
 });
 
 app.post("/", function(req, res) {
-    let item = req.body.newItem;
+    const itemName = req.body.newItem;
+    const item = new Item({
+        name: itemName,
+    });
     if (req.body.list === "work") {
-        workItems.push(item);
+        item.save();
         res.redirect("/work");
     } else {
-        items.push(item);
+        item.save();
         res.redirect("/");
     }
 });
